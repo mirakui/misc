5f74f4d77e74ae07bd68998ab17e7045377e16ec
Issei Naruta <naruta@cookpad.com>
2018-01-07 22:22:14 +0900

    Implement file cache

--- geo_distance/geo_distance.rb
+++ geo_distance/geo_distance.rb
@@ -1,16 +1,53 @@
 require 'geocoder'
+require 'yaml'
+
+class GeoCache
+  CACHE_PATH = '.geocoder_cache'
+
+  def initialize
+    unless @cache
+      if File.exist?(CACHE_PATH)
+        @cache = YAML.load(open(CACHE_PATH).read)
+      else
+        @cache = {}
+      end
+    end
+  end
+
+  def save
+    open(CACHE_PATH, 'w+') do |f|
+      f.write @cache.to_yaml
+    end
+  end
+
+  def set(addr, geometry)
+    @cache[addr] = geometry
+    save
+  end
+
+  def get(addr)
+    @cache[addr]
+  end
+end
 
 def geo_search(addr)
-  @geo_cache ||= {}
-  @geo_cache[addr] ||= Geocoder.search(addr)
+  @geo_cache ||= GeoCache.new
+  cached_geom = @geo_cache.get(addr)
+  if cached_geom
+    cached_geom
+  else
+    geom = Geocoder.search(addr).first.geometry
+    @geo_cache.set addr, geom
+    geom
+  end
 end
 
 def calc_distance(addr0, addr1)
-  locs = []
-  locs[0] = geo_search addr0
-  locs[1] = geo_search addr1
-  latlngs = locs.map do |l|
-    geo = l.first.geometry['location']
+  geoms = []
+  geoms[0] = geo_search addr0
+  geoms[1] = geo_search addr1
+  latlngs = geoms.map do |l|
+    geo = l['location']
     [geo['lat'], geo['lng']]
   end
   Geocoder::Calculations.distance_between *latlngs, units: :km
