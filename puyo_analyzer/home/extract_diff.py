6d2962bea7e3918166fca70503098a8f8d088bba
Issei Naruta <naruta@cookpad.com>
2020-02-03 23:16:41 +0900

    ThumbnailsAnalyzer

--- puyo_analyzer/home/extract_diff.py
+++ puyo_analyzer/home/extract_diff.py
@@ -7,65 +7,70 @@ import glob
 import os
 import shutil
 
-diff_threshold = 0.7
-mt_x_threshold = 0.6
-mt_yatta_threshold = 0.7
+class ThumbnailsAnalyzer:
+    def __init__(self):
+        self.mt_tsumo_threshold = 0.7
+        self.mt_x_threshold = 0.6
+        self.mt_yatta_threshold = 0.7
+        self.tsumo_frames_threshold = 4
+        self.img_x_path = 'img/x.png'
+        self.img_yatta_path = 'img/yatta.png'
 
-img_x = skimage.color.rgb2gray(skimage.io.imread('img/x.png'))
-img_yatta = skimage.color.rgb2gray(skimage.io.imread('img/yatta.png'))
-src_files = glob.glob('/mnt/vol/30fps-02/dst*.jpg')
-dst_dir = '/mnt/vol/diff/'
-img1 = None
-keyframe_path = None
-is_ren = False
-is_yatta = False
+    def analyze(self, src_files, dst_dir):
+        img_x = skimage.color.rgb2gray(skimage.io.imread(self.img_x_path))
+        img_yatta = skimage.color.rgb2gray(skimage.io.imread(self.img_yatta_path))
 
-seq_count = 0
-
-for f in sorted(src_files):
-    img0 = img1
-    img1 = skimage.io.imread(f)
-    img1 = skimage.color.rgb2gray(img1)
-
-    if img0 is None:
-        continue
-    if keyframe_path is None:
-        keyframe_path = f
-
-    mt_x_result = skimage.feature.match_template(img1[294:314, 116:225], img_x)
-    mt_x_score = np.max(mt_x_result)
-    print('DEBUG [mt_x_score]', f, mt_x_score)
-    if mt_x_score >= mt_x_threshold:
-        if not is_ren:
-            print('Ren:', f, mt_x_score)
-            basename = os.path.basename(f)
-            dst_path = dst_dir + basename
-            shutil.copy(f, dst_path)
-            is_ren = True
-    else:
+        img1 = None
         is_ren = False
-
-    mt_yatta_result = skimage.feature.match_template(img1, img_yatta)
-    mt_yatta_score = np.max(mt_yatta_result)
-    if mt_yatta_score >= mt_yatta_threshold:
-        if not is_yatta:
-            print('Yatta:', f, mt_yatta_score)
-            basename = os.path.basename(f)
-            dst_path = dst_dir + basename
-            shutil.copy(f, dst_path)
-            is_yatta = True
-    else:
         is_yatta = False
 
-    (score, diff) = skimage.metrics.structural_similarity(img0[45:147, 232:283], img1[45:147, 232:283], full=True)
-
-    if score <= diff_threshold:
-        seq_count += 1
-        if seq_count >= 4:
-            basename = os.path.basename(keyframe_path)
-            print('Tsumo:', keyframe_path, score)
-            dst_path = dst_dir + basename
-            shutil.copy(keyframe_path, dst_path)
-    else:
         seq_count = 0
-        keyframe_path = f
+
+        for f in src_files:
+            img0 = img1
+            img1 = skimage.io.imread(f)
+            img1 = skimage.color.rgb2gray(img1)
+            basename = os.path.basename(f)
+
+            if img0 is None:
+                continue
+
+            mt_x_result = skimage.feature.match_template(img1[294:314, 116:225], img_x)
+            mt_x_score = np.max(mt_x_result)
+            # print('DEBUG [mt_x_score]', f, mt_x_score)
+            if mt_x_score >= self.mt_x_threshold:
+                if not is_ren:
+                    print('Ren:', basename, mt_x_score)
+                    shutil.copy(f, dst_dir + basename)
+                    is_ren = True
+                    continue
+            else:
+                is_ren = False
+
+            mt_yatta_result = skimage.feature.match_template(img1, img_yatta)
+            mt_yatta_score = np.max(mt_yatta_result)
+            if mt_yatta_score >= self.mt_yatta_threshold:
+                if not is_yatta:
+                    print('Yatta:', basename, mt_yatta_score)
+                    shutil.copy(f, dst_dir + basename)
+                    is_yatta = True
+                    continue
+            else:
+                is_yatta = False
+
+            (score, diff) = skimage.metrics.structural_similarity(img0[45:147, 232:283], img1[45:147, 232:283], full=True)
+
+            if score <= self.mt_tsumo_threshold:
+                seq_count += 1
+                if seq_count >= self.tsumo_frames_threshold:
+                    print('Tsumo:', keyframe_path, score)
+                    dst_path = dst_dir + os.path.basename(keyframe_path)
+                    shutil.copy(keyframe_path, dst_path)
+            else:
+                seq_count = 0
+                keyframe_path = f
+
+src_files = sorted(glob.glob('/mnt/vol/30fps-02/dst*.jpg'))
+dst_dir = '/mnt/vol/diff/'
+analyzer = ThumbnailsAnalyzer()
+analyzer.analyze(src_files=src_files, dst_dir=dst_dir)
