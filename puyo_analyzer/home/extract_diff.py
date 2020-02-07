f45ca263aeeb1149d0908fad3699ab5c56ff83c6
Issei Naruta <naruta@cookpad.com>
2020-02-08 00:08:15 +0900

    Add frame_ratio

--- puyo_analyzer/home/extract_diff.py
+++ puyo_analyzer/home/extract_diff.py
@@ -2,30 +2,30 @@ import skimage
 import skimage.io
 import skimage.metrics
 import skimage.feature
+import skimage.transform
 import numpy as np
 import glob
 import os
 import shutil
 
 class Rect:
-    def __init__(self, y1, y2, x1, x2, ratio=1.0):
-        self.x1 = int(x1 * ratio)
-        self.x2 = int(x2 * ratio)
-        self.y1 = int(y1 * ratio)
-        self.y2 = int(y2 * ratio)
+    def __init__(self, y1, y2, x1, x2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
 
     def crop(self, img):
         return img[self.y1:self.y2, self.x1:self.x2]
 
 class TsumoFrameDetector:
-    def __init__(self, dst_dir):
+    def __init__(self, frame_ratio):
         self.mt_tsumo_threshold = 0.7
-        self.tsumo_frames_threshold = 4
+        self.tsumo_frames_threshold = 8 * frame_ratio
         self.mt_tsumo_rect = Rect(x1=232, x2=283, y1=45, y2=147)
         self.tsumo_seq_count = 0
         self.keyframe_path = None
         self.img_field_prev = None
-        self.dst_dir = dst_dir
 
     # ネクスト領域で一定フレーム連続して前フレームとの差が大きかったらツモアニメーション中
     # おじゃまぷよが飛んでいくエフェクトがネクスト領域を横断することがあるが、ツモアニメーションよりフレーム数が少ない
@@ -45,7 +45,6 @@ class TsumoFrameDetector:
             if self.tsumo_seq_count >= self.tsumo_frames_threshold:
                 print('Tsumo:', self.keyframe_path, score)
                 basename = os.path.basename(self.keyframe_path)
-                shutil.copy(self.keyframe_path, self.dst_dir + basename)
                 return { 'file': basename, 'type': 'tsumo', 'player': 0 }
         else:
             self.tsumo_seq_count = 0
@@ -54,8 +53,7 @@ class TsumoFrameDetector:
 
 
 class RenFrameDetector:
-    def __init__(self, dst_dir):
-        self.dst_dir = dst_dir
+    def __init__(self):
         self.mt_x_threshold = 0.6
         self.img_x_path = 'img/x.png'
         self.mt_x_rect = Rect(x1=116, x2=225, y1=294, y2=314)
@@ -72,13 +70,11 @@ class RenFrameDetector:
         score = np.max(result)
         if score >= self.mt_x_threshold:
             print('Ren:', basename, score)
-            shutil.copy(f, dst_dir + basename)
             return { 'file': basename, 'type': 'ren', 'player': 0 }
 
 
 class YattaFrameDetector:
-    def __init__(self, dst_dir):
-        self.dst_dir = dst_dir
+    def __init__(self):
         self.mt_yatta_threshold = 0.7
         self.img_yatta_path = 'img/yatta.png'
         self.img_yatta = skimage.color.rgb2gray(skimage.io.imread(self.img_yatta_path))
@@ -91,7 +87,6 @@ class YattaFrameDetector:
         score = np.max(result)
         if score >= self.mt_yatta_threshold:
             print('Yatta:', basename, score)
-            shutil.copy(f, self.dst_dir + basename)
             return { 'file': basename, 'type': 'eor' }
         else:
             return None
@@ -101,16 +96,20 @@ class ThumbnailsAnalyzer:
     def __init__(self):
         pass
 
-    def analyze(self, src_files, dst_dir):
+    def analyze(self, src_files, frame_ratio=1.0):
         is_ren = False
         is_yatta = False
         result = { 'frames': [] }
-        tsumo_frame_detector = TsumoFrameDetector(dst_dir=dst_dir)
-        ren_frame_detector = RenFrameDetector(dst_dir=dst_dir)
-        yatta_frame_detector = YattaFrameDetector(dst_dir=dst_dir)
+
+        tsumo_frame_detector = TsumoFrameDetector(frame_ratio=frame_ratio)
+        ren_frame_detector = RenFrameDetector()
+        yatta_frame_detector = YattaFrameDetector()
 
         for f in src_files:
             img_field = skimage.io.imread(f) # ツモ順分析のためにカラー版も残しておく
+            # 640x360(原寸の半分)じゃないと今のところ動かない(テンプレがそのサイズのため)
+            if img_field.shape[1] != 640:
+                img_field = skimage.transform.resize(img_field, (360, 640))
             img_field_gray = skimage.color.rgb2gray(img_field)
             basename = os.path.basename(f)
 
@@ -138,8 +137,7 @@ class ThumbnailsAnalyzer:
         return result
 
 src_files = sorted(glob.glob('/mnt/vol/30fps-02/dst*.jpg'))
-dst_dir = '/mnt/vol/diff/'
 analyzer = ThumbnailsAnalyzer()
-result = analyzer.analyze(src_files=src_files, dst_dir=dst_dir)
+result = analyzer.analyze(src_files=src_files, frame_ratio=0.5)
 
 print(result)
