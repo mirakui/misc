315dfc074997f1abcf352d88cf726875a7b9fa25
Issei Naruta <naruta@cookpad.com>
2020-02-09 23:53:25 +0900

    scaleを実装したがなんか動いてない気がする

--- puyo_analyzer/home/extract_diff.py
+++ puyo_analyzer/home/extract_diff.py
@@ -11,11 +11,11 @@ from enum import Enum
 import json
 
 class Rect:
-    def __init__(self, y1, y2, x1, x2):
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
+    def __init__(self, y1, y2, x1, x2, scale=1.0):
+        self.x1 = int(x1 * scale)
+        self.x2 = int(x2 * scale)
+        self.y1 = int(y1 * scale)
+        self.y2 = int(y2 * scale)
 
     def crop(self, img):
         return img[self.y1:self.y2, self.x1:self.x2]
@@ -24,13 +24,32 @@ class Player(Enum):
     P1 = 0
     P2 = 1
 
+
+def load_template(path, scale, gray=True):
+    img = skimage.io.imread(path)
+    if gray:
+        img = skimage.color.rgb2gray(img)
+    img = resize_template(img, scale=scale)
+    return img
+
+def resize_template(img, scale):
+    if scale < 1.0:
+        s = img.shape
+        return skimage.transform.resize(
+            img,
+            (int(s[0]*scale), int(s[1]*scale))
+        )
+    else:
+        return img
+
+
 class TsumoFrameDetector:
-    def __init__(self, frame_ratio, player):
+    def __init__(self, player, screen_ratio=1.0):
         self.player = player
         if player == Player.P1:
-            self.mt_tsumo_rect = Rect(x1=232, x2=283, y1=45, y2=147)
+            self.mt_tsumo_rect = Rect(x1=232, x2=283, y1=45, y2=147, scale=screen_ratio)
         elif player == Player.P2:
-            self.mt_tsumo_rect = Rect(x1=357, x2=408, y1=45, y2=147)
+            self.mt_tsumo_rect = Rect(x1=357, x2=408, y1=45, y2=147, scale=screen_ratio)
         else:
             raise ValueError('Unexpected player', player)
 
@@ -53,17 +72,16 @@ class TsumoFrameDetector:
 
 
 class RenFrameDetector:
-    def __init__(self, player):
+    def __init__(self, player, screen_ratio=1.0):
         self.img_x_path = 'img/x.png'
+        self.img_x = load_template(path=self.img_x_path, scale=screen_ratio)
         self.player = player
         if player == Player.P1:
-            self.mt_x_rect = Rect(x1=116, x2=225, y1=294, y2=314)
+            self.mt_x_rect = Rect(x1=116, x2=225, y1=294, y2=314, scale=screen_ratio)
         elif player == Player.P2:
-            self.mt_x_rect = Rect(x1=415, x2=524, y1=294, y2=314)
+            self.mt_x_rect = Rect(x1=415, x2=524, y1=294, y2=314, scale=screen_ratio)
         else:
             raise ValueError('Unexpected player', player)
-
-        self.img_x = skimage.color.rgb2gray(skimage.io.imread(self.img_x_path))
 
     # "x" が得点領域に表示されていたら連鎖アニメーション中
     def detect_x(self, img_field):
@@ -76,12 +94,12 @@ class RenFrameDetector:
 
 
 class YattaFrameDetector:
-    def __init__(self):
+    def __init__(self, screen_ratio=1.0):
         self.img_yatta_path = 'img/yatta.png'
-        self.img_yatta = skimage.color.rgb2gray(skimage.io.imread(self.img_yatta_path))
+        self.img_yatta = load_template(path=self.img_yatta_path, scale=screen_ratio)
         self.mt_yatta_rects = [
-            Rect(x1=96, x2=210, y1=132, y2=160),
-            Rect(x1=430, x2=544, y1=132, y2=160)
+            Rect(x1=96, x2=210, y1=132, y2=160, scale=screen_ratio),
+            Rect(x1=430, x2=544, y1=132, y2=160, scale=screen_ratio)
         ]
 
     # "やった！" が表示されていたらラウンド終了
@@ -98,10 +116,11 @@ class YattaFrameDetector:
 
 
 class WinFrameDetector:
-    def __init__(self):
+    def __init__(self, screen_ratio=1.0):
         self.img_win_path = 'img/win.png'
-        self.img_win = skimage.color.rgb2gray(skimage.io.imread(self.img_win_path))
-        self.mt_win_rect = Rect(x1=309, x2=332, y1=324, y2=335)
+        self.img_win = load_template(path=self.img_win_path, scale=screen_ratio)
+
+        self.mt_win_rect = Rect(x1=309, x2=332, y1=324, y2=335, scale=screen_ratio)
 
     # WIN の文字が画面中央下に表示されていたらゲーム中
     def detect_win(self, img_field):
@@ -133,25 +152,28 @@ class MemoCounter:
 
 
 class ThumbnailsAnalyzer:
-    def __init__(self, frame_ratio=1.0):
+    def __init__(self, frame_ratio=1.0, screen_ratio=1.0):
         self.frame_ratio = frame_ratio
+        self.screen_ratio = screen_ratio
         pass
 
     def detect(self, src_files):
         players = (Player.P1, Player.P2)
-        tsumo_frame_detectors = { p: TsumoFrameDetector(frame_ratio=self.frame_ratio, player=p) for p in players }
-        ren_frame_detectors = { p: RenFrameDetector(player=p) for p in players }
-        yatta_frame_detector = YattaFrameDetector()
-        win_frame_detector = WinFrameDetector()
+        tsumo_frame_detectors = {
+            p: TsumoFrameDetector(player=p, screen_ratio=self.screen_ratio) for p in players
+        }
+        ren_frame_detectors = {
+            p: RenFrameDetector(player=p, screen_ratio=self.screen_ratio) for p in players
+        }
+        yatta_frame_detector = YattaFrameDetector(screen_ratio=self.screen_ratio)
+        win_frame_detector = WinFrameDetector(screen_ratio=self.screen_ratio)
 
         result = { 'frames': [] }
 
         for f in src_files:
             frame = { 'file': os.path.basename(f) }
             img_field = skimage.io.imread(f) # ツモ順分析のためにカラー版も残しておく
-            # 640x360(原寸の半分)じゃないと今のところ動かない(テンプレがそのサイズのため)
-            if img_field.shape[1] != 640:
-                img_field = skimage.transform.resize(img_field, (360, 640))
+            resize_template(img_field, scale=self.screen_ratio)
             img_field_gray = skimage.color.rgb2gray(img_field)
 
             frame['yatta'] = int(yatta_frame_detector.detect_yatta(img_field_gray) * 1000)
@@ -237,7 +259,7 @@ class ThumbnailsAnalyzer:
 
 
 src_files = sorted(glob.glob('/mnt/vol/30fps-02/dst*.jpg'))
-analyzer = ThumbnailsAnalyzer(frame_ratio=0.5)
+analyzer = ThumbnailsAnalyzer(frame_ratio=0.5, screen_ratio=1.0)
 #result = analyzer.analyze(src_files=src_files, frame_ratio=0.5)
 
 detected_json_path = 'detected.json'
