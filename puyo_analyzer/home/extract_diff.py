264d8cb099db6ba80a252915d81e8a3e1a1514b2
Issei Naruta <naruta@cookpad.com>
2020-02-09 22:19:22 +0900

    Remove unused

--- puyo_analyzer/home/extract_diff.py
+++ puyo_analyzer/home/extract_diff.py
@@ -26,8 +26,6 @@ class Player(Enum):
 
 class TsumoFrameDetector:
     def __init__(self, frame_ratio, player):
-        self.mt_tsumo_threshold = 0.7
-        self.tsumo_frames_threshold = 8 * frame_ratio
         self.player = player
         if player == Player.P1:
             self.mt_tsumo_rect = Rect(x1=232, x2=283, y1=45, y2=147)
@@ -36,34 +34,10 @@ class TsumoFrameDetector:
         else:
             raise ValueError('Unexpected player', player)
 
-        self.tsumo_seq_count = 0
-        self.keyframe_path = None
         self.img_field_prev = None
 
     # ネクスト領域で一定フレーム連続して前フレームとの差が大きかったらツモアニメーション中
     # おじゃまぷよが飛んでいくエフェクトがネクスト領域を横断することがあるが、ツモアニメーションよりフレーム数が少ない
-    def detect(self, img_field, f):
-        if self.img_field_prev is None:
-            self.img_field_prev = img_field
-            return None
-
-        (score, diff) = skimage.metrics.structural_similarity(
-            self.mt_tsumo_rect.crop(self.img_field_prev),
-            self.mt_tsumo_rect.crop(img_field),
-            full=True
-        )
-        self.img_field_prev = img_field
-        if score <= self.mt_tsumo_threshold:
-            self.tsumo_seq_count += 1
-            if self.tsumo_seq_count >= self.tsumo_frames_threshold:
-                print('Tsumo:', self.keyframe_path, self.player.value, score)
-                basename = os.path.basename(self.keyframe_path)
-                return { 'file': basename, 'type': 'tsumo', 'player': self.player.value }
-        else:
-            self.tsumo_seq_count = 0
-            self.keyframe_path = f
-            return None
-
     def detect_diff(self, img_field):
         if self.img_field_prev is None:
             self.img_field_prev = img_field
@@ -80,7 +54,6 @@ class TsumoFrameDetector:
 
 class RenFrameDetector:
     def __init__(self, player):
-        self.mt_x_threshold = 0.6
         self.img_x_path = 'img/x.png'
         self.player = player
         if player == Player.P1:
@@ -93,18 +66,6 @@ class RenFrameDetector:
         self.img_x = skimage.color.rgb2gray(skimage.io.imread(self.img_x_path))
 
     # "x" が得点領域に表示されていたら連鎖アニメーション中
-    def detect(self, img_field, f):
-        basename = os.path.basename(f)
-
-        result = skimage.feature.match_template(
-            self.mt_x_rect.crop(img_field),
-            self.img_x
-        )
-        score = np.max(result)
-        if score >= self.mt_x_threshold:
-            print('Ren:', basename, self.player.value, score)
-            return { 'file': basename, 'type': 'ren', 'player': self.player.value }
-
     def detect_x(self, img_field):
         result = skimage.feature.match_template(
             self.mt_x_rect.crop(img_field),
@@ -116,22 +77,10 @@ class RenFrameDetector:
 
 class YattaFrameDetector:
     def __init__(self):
-        self.mt_yatta_threshold = 0.7
         self.img_yatta_path = 'img/yatta.png'
         self.img_yatta = skimage.color.rgb2gray(skimage.io.imread(self.img_yatta_path))
 
     # "やった！" が表示されていたらラウンド終了
-    def detect(self, img_field, f):
-        basename = os.path.basename(f)
-
-        result = skimage.feature.match_template(img_field, self.img_yatta)
-        score = np.max(result)
-        if score >= self.mt_yatta_threshold:
-            print('Yatta:', basename, score)
-            return { 'file': basename, 'type': 'eor' }
-        else:
-            return None
-
     def detect_yatta(self, img_field):
         result = skimage.feature.match_template(img_field, self.img_yatta)
         score = np.max(result)
@@ -246,52 +195,6 @@ class ThumbnailsAnalyzer:
 
         return result
 
-
-    def analyze2(self, src_files, frame_ratio=1.0):
-        is_ren = { Player.P1: False, Player.P2: False }
-        is_yatta = False
-        result = { 'frames': [] }
-        players = (Player.P1, Player.P2)
-        #players = (Player.P1,)
-        skip_flag = True
-
-        tsumo_frame_detectors = { p: TsumoFrameDetector(frame_ratio=frame_ratio, player=p) for p in players }
-        ren_frame_detectors = { p: RenFrameDetector(player=p) for p in players }
-        yatta_frame_detector = YattaFrameDetector()
-
-        for f in src_files:
-            basename = os.path.basename(f)
-            img_field = skimage.io.imread(f) # ツモ順分析のためにカラー版も残しておく
-            # 640x360(原寸の半分)じゃないと今のところ動かない(テンプレがそのサイズのため)
-            if img_field.shape[1] != 640:
-                img_field = skimage.transform.resize(img_field, (360, 640))
-            img_field_gray = skimage.color.rgb2gray(img_field)
-
-            if not is_yatta:
-                # TODO 数フレーム yatta を表示しないと次のフェーズにいかないようにする
-                yatta_frame = yatta_frame_detector.detect(img_field_gray, f)
-                if yatta_frame:
-                    result['frames'].append(yatta_frame)
-                    is_yatta = True
-                    continue
-
-            for p in players:
-                ren_frame = ren_frame_detectors[p].detect(img_field_gray, f)
-                if ren_frame:
-                    if not is_ren[p]:
-                        result['frames'].append(ren_frame)
-                        is_ren[p] = True
-                        continue
-                else:
-                    is_ren[p] = False
-
-                # TODO ラウンド開始の判定
-                tsumo_frame = tsumo_frame_detectors[p].detect(img_field_gray, f)
-                if tsumo_frame:
-                    result['frames'].append(tsumo_frame)
-                    continue
-
-        return result
 
 src_files = sorted(glob.glob('/mnt/vol/30fps-02/dst*.jpg'))
 analyzer = ThumbnailsAnalyzer(frame_ratio=0.5)
