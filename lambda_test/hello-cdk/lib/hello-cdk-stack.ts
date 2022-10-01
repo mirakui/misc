f5ed2e85345e819ccb52ae7ad4e75c958c8ebfdb
Issei Naruta <naruta@cookpad.com>
2022-10-02 08:47:02 +0900

    cdk init

--- lambda_test/hello-cdk/lib/hello-cdk-stack.ts
+++ lambda_test/hello-cdk/lib/hello-cdk-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_lambda as lambda } from 'aws-cdk-lib';
 
 export class HelloCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new lambda.Function(this, 'NaiveLambda', {
+    runtime: lambda.Runtime.NODEJS_14_X,
+    handler: 'index.handler',
+    code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/')),
+});
   }
 }
